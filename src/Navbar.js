d2d420f2476e52e3b47d5a1ef5667d6604ddbe02
VinhDangQuang <45581115+VinhDangQuang@users.noreply.github.com>
2019-12-01 16:38:25 +0700

    add Navbar and footer palette

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 import Slider from "rc-slider";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
-import Snackbar from '@material-ui/core/Snackbar';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
 
@@ -31,29 +31,31 @@ class Navbar extends Component {
   };
 
   handleCloseNavbar = () => {
-    this.setState({ open: false })
-  }
+    this.setState({ open: false });
+  };
 
   render() {
-    const { level, changeLevel } = this.props;
+    const { level, changeLevel, showSlider } = this.props;
     const { format, open } = this.state;
     return (
       <header className="Navbar">
         <div className="logo">
           <Link to="/">reactcolorpicker</Link>
         </div>
-        <div className="slider-container">
-          <span>Level: {level} </span>
-          <div className="slider">
-            <Slider
-              defaultValue={level}
-              min={100}
-              max={900}
-              onAfterChange={changeLevel}
-              step={100}
-            />
+        {showSlider && (
+          <div className="slider-container">
+            <span>Level: {level} </span>
+            <div className="slider">
+              <Slider
+                defaultValue={level}
+                min={100}
+                max={900}
+                onAfterChange={changeLevel}
+                step={100}
+              />
+            </div>
           </div>
-        </div>
+        )}
         <div className="select-container">
           <Select value={format} onChange={this.handleFormatChange}>
             <MenuItem value="hex">HEX - #ffffff</MenuItem>
--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
+import Navbar from "./Navbar";
+import PaletteFooter from "./PaletteFooter";
 
 class SingleColorPalette extends Component {
   constructor(props) {
     super(props);
     this._shades = this.gatherShades(this.props.palette, this.props.colorId);
+    this.state = { format: "hex" };
   }
+
+  changeFormat = format => {
+    this.setState({ format: format });
+  };
+
   gatherShades = (palette, colorToFilterBy) => {
     let shades = [];
     let allColors = palette.colors;
-    console.log("palette.colors", palette.colors)
+    console.log("palette.colors", palette.colors);
     for (let key in allColors) {
       shades = shades.concat(
         allColors[key].filter(color => color.id === colorToFilterBy)
@@ -18,17 +26,16 @@ class SingleColorPalette extends Component {
     return shades.slice(1);
   };
   render() {
+    const { paletteName, emoji } = this.props.palette;
+    const { format } = this.state;
     const colorBoxes = this._shades.map(color => (
-      <ColorBox
-        key={color.id}
-        background={color.hex}
-        name={color.name}
-      />
+      <ColorBox key={color.id} background={color[format]} name={color.name} />
     ));
     return (
       <div className="Palette">
-        <h1>Single Color Palette</h1>
+        <Navbar handleChange={this.changeFormat} />
         <div className="Palette-colors">{colorBoxes}</div>
+        <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
     );
   }
